fixed bugs in derivatives for cartpole and pendulum

demos/model_based/cartpole_liftup.py
@@ -1,4 +1,4 @@
-## This is a demo that generates a plan for the cartpole env using ilqr
+## This is a demo that generates a plan to lift a cartpole up
 ## Author : Avadesh Meduri
 ## Date : 25/11/2020
 
@@ -17,12 +17,12 @@ from model_based.ilqr import ILQR
 
 
 # initialising the env
-env = Cartpole(5, 1, 5)
+env = Cartpole(10, 5, 5)
 
 init_x = 0
-init_theta = 140*(np.pi/180)
+init_theta = 50*(np.pi/180)
 init_xd = 0
-init_theta_d = 0.5
+init_theta_d = 0
 
 env.reset_cartpole(init_x, init_theta, init_xd, init_theta_d)
 
@@ -36,18 +36,18 @@ theta_d_terminal = 0
 state_terminal = np.array([x_terminal, theta_terminal, xd_terminal, theta_d_terminal])
 
 Q_t = 1e-5*np.identity(4)
-Q_t[0,0], Q_t[1,1], Q_t[2,2], Q_t[3,3] = [1e-8, 1.5e+2, 1e-8, 1e-8]
+Q_t[0,0], Q_t[1,1], Q_t[2,2], Q_t[3,3] = [1e+2, 2e+2, 1e-8, 1e-3]
 Q_f = Q_t
-
-R_t = 1e-5
+Q_f[3,3] = 1e-1
+R_t = 1e-3
 
 ptc = QuadraticTrackingCost(env, state_terminal, Q_t)
 tpc = TerminalQuadraticTrackingCost(env, state_terminal, Q_f)
 crc = ControlRegularizerCost(env, R_t)
 # initialising ilqr
 dt = 0.01
-T = 2.
-no_iterations = 100
+T = 4.
+no_iterations = 80
 ilqr = ILQR(env, dt)
 ilqr.initialize(T, x_init)
 env.dt = dt
@@ -69,5 +69,5 @@ for t in range(horizon):
     torque = np.matmul(K_arr[int(t//r)],(state - x_des[:,int(t//r)]).transpose()) + k_arr[:,int(t//r)]
     env.step_cartpole(float(torque))
 
-env.animate(15)
+env.animate(25)
 env.plot()

demos/model_based/inv_pend.py
@@ -18,22 +18,23 @@ from model_based.ilqr import ILQR
 
 # initialising the env
 env = OneDOFManipulator(1, 1)
-theta_init = 60*(np.pi/180)
+theta_init = 0*(np.pi/180)
 env.reset_manipulator(theta_init,0)
 x_init = np.array([env.get_joint_position(), env.get_joint_velocity()])
 # defining the cost
 x_terminal = np.array([180*(np.pi/180), 0]) 
-Q_t = 10.0*np.identity(2)
-Q_f = 10*np.identity(2)
-R_t = 1e-5
+Q_t = np.identity(2)
+Q_t[0,0], Q_t[1,1] = [100, 20]
+Q_f = Q_t
+R_t = 1e-2
 
 ptc = QuadraticTrackingCost(env, x_terminal, Q_t)
 tpc = TerminalQuadraticTrackingCost(env, x_terminal, Q_f)
 crc = ControlRegularizerCost(env, R_t)
 # initialising ilqr
 dt = 0.01
-T = 2.5
-no_iterations = 80
+T = 2
+no_iterations = 20
 ilqr = ILQR(env, dt)
 ilqr.initialize(T, x_init)
 env.dt = dt
@@ -58,4 +59,4 @@ for t in range(horizon):
     env.step_manipulator(float(torque), True)
 
 env.animate(1)
-env.plot()
+# env.plot()

demos/robot_env/cartpole.py
@@ -16,10 +16,10 @@ sys.path.append(cdir)
 import numpy as np
 from robot_env.cartpole import Cartpole
 
-env = Cartpole(2, 0.5, 2)
+env = Cartpole(5, 1, 5)
 
 init_x = 0
-init_theta = 0*(np.pi/180)
+init_theta = 250*(np.pi/180)
 init_xd = 0
 init_theta_d = 0
 
@@ -29,7 +29,9 @@ horizon = 10000 # duration of simulation steps
 
 for t in range(horizon):
     
-    torque = -5.0
+    state = env.get_states()
+    # torque = -20*(state[0] - 5*np.sin(np.pi*0.001*t))
+    torque = 0
     env.step_cartpole(torque)
 
 env.animate()

srcpy/model_based/ilqr.py
@@ -8,14 +8,16 @@ from matplotlib import pyplot as plt
 
 class ILQR:
 
-    def __init__(self, env, dt):
+    def __init__(self, env, dt, beta = 0.75):
         '''
         Input:
             env : the dynamics of the system for which trajectory is generated
             dt : discretization
+            beta : line seach reduction factor
         '''
         self.env = env
         self.dt = dt
+        self.beta = beta
         self.iter_cost_arr = [] # stores cost after each forward pass
         self.iter_traj_arr = [] # stores new trajectory after every iteration
 
@@ -122,16 +124,42 @@ class ILQR:
         '''
         This function runs the forward pass for the ilqr
         '''
-        epi_cost = 0
-        for t in range(self.n):
-            self.u[:,t] += np.matmul(self.K[t], (self.x[:,t] - self.x_nom[:,t])) + self.k[t]
-            self.x[:,t+1] = self.env.integrate_dynamics(self.x[:,t], self.u[:,t])
-            if np.isnan(self.x[:,t+1]).any():
-                assert False
+        alpha = 1 # step length
+        x_base = self.x.copy()
+        u_base = self.u.copy()
+        return_alpha = False
+        old_cost = 1e+1000
+        z = 0
+        # print('\n')
+        while alpha > 0.001 and z < 20:
+            epi_cost = 0
+            self.x = x_base.copy()
+            self.u = u_base.copy()
+            for t in range(self.n):
+                self.u[:,t] = u_base[:,t] + np.matmul(self.K[t], (self.x[:,t] - self.x_nom[:,t])) + alpha*self.k[t]
+                self.x[:,t+1] = self.env.integrate_dynamics(self.x[:,t], self.u[:,t])
+                if np.isnan(self.x[:,t+1]).any():
+                    assert False
 
-            epi_cost += self.compute_running_cost(t) 
-        
-        epi_cost += self.compute_terminal_cost(t+1)
+                epi_cost += self.compute_running_cost(t) 
+            epi_cost += self.compute_terminal_cost(t+1)
+            z += 1
+            # print(epi_cost, alpha)
+
+            if len(self.iter_cost_arr) == 0:
+                break
+            elif return_alpha:
+                break
+            elif epi_cost < self.iter_cost_arr[-1]:
+                break
+            elif old_cost < epi_cost:
+                alpha = (1/self.beta)*alpha
+                return_alpha = True
+                old_cost = epi_cost
+            else:
+                alpha = self.beta*alpha
+                old_cost = epi_cost
+        # print('done \n')
         self.iter_cost_arr.append(float(epi_cost))
         self.iter_traj_arr.append(self.x)
         self.x_nom = self.x.copy()
@@ -166,13 +194,15 @@ class ILQR:
         '''
         This function runs ilqr and returs optimal trajectory
         '''
-        for n in range(no_iterations):
-            self.forward_pass()
-            self.backward_pass()
-            print("finished iteration {} and the cost is {}".format(n, self.iter_cost_arr[-1]), end='\r')
-            
         self.forward_pass()
-
+        for n in range(no_iterations):
+            self.backward_pass()
+            self.forward_pass()
+            print("finished iteration {} and the cost is {}".format(n, self.iter_cost_arr[n+1]), end='\r')
+            
+            if len(self.iter_cost_arr) > 2:
+                if self.iter_cost_arr[-2] < self.iter_cost_arr[-1]:
+                    break
 
         return self.x_nom, self.K, self.u
 
@@ -190,7 +220,7 @@ class ILQR:
         ax[1].grid()
         ax[1].legend()
 
-        ax[2].plot(self.iter_cost_arr, label = "cost")
+        ax[2].plot(self.iter_cost_arr[1:], label = "cost")
         ax[2].grid()
         ax[2].legend()
 

srcpy/robot_env/cartpole.py
@@ -61,12 +61,13 @@ class Cartpole:
 
         den = (self.mc + self.mp*np.sin(theta)**2)    
         A_x[2,1] = (-self.mp*np.sin(2*theta)*(actions + self.mp*np.sin(theta)*(self.lp*theta_d**2 + self.g*np.cos(theta))))/den**2 \
-                    + (actions + self.lp*self.mp*np.cos(theta)*theta_d**2 + self.mp*self.g*np.cos(2*theta))/den
+                    + (self.lp*self.mp*np.cos(theta)*theta_d**2 + self.mp*self.g*np.cos(2*theta))/den
         
         A_x[2,3] = 2*self.mp*self.lp*theta_d*np.sin(theta)/den
 
-        A_x[3,1] = (-self.mp*np.sin(2*theta)*(-actions*np.cos(theta) - 0.5*self.mp*self.lp*np.sin(2*theta_d)*theta_d**2 - (self.mc + self.mp)*self.g*np.sin(theta)))/(self.lp*den**2) + \
-                    (actions*np.sin(theta) - self.mp*self.lp*np.cos(2*theta_d)*theta_d**2 - (self.mc + self.mp)*self.g*np.cos(theta))/(self.lp*den)  
+        A_x[3,1] = (self.mp*np.sin(2*theta)*(actions*np.cos(theta) + 0.5*self.mp*self.lp*(theta_d**2)*np.sin(2*theta) + (self.mc + self.mp)*self.g*np.sin(theta)))/(self.lp*(den**2)) + \
+                    (actions*np.sin(theta) - self.mp*self.lp*np.cos(2*theta)*theta_d**2 - (self.mc + self.mp)*self.g*np.cos(theta))/(self.lp*den)
+        A_x[3,3] = (-self.mp*self.lp*theta_d*(np.sin(2*theta)))/(self.lp*den)
 
         return A_x
 
@@ -99,8 +100,8 @@ class Cartpole:
         '''
         B_lin = np.zeros((4,1))
         den = (self.mc + self.mp*np.sin(state[1])**2)
-        B_lin[2] = (1/den)
-        B_lin[3] = (1/(self.lp*den))*(-np.cos(state[1]))
+        B_lin[2] = (dt/den)
+        B_lin[3] = (dt/(self.lp*den))*(-np.cos(state[1]))
 
         return B_lin
 
@@ -141,7 +142,7 @@ class Cartpole:
         sim_data = self.sim_data[:,::freq]
 
         fig = plt.figure()
-        ax = plt.axes(xlim=(-self.length - 20, self.length+ 20), ylim=(-self.length - 20, self.length + 20))
+        ax = plt.axes(xlim=(-self.length - 10, self.length+ 10), ylim=(-self.length - 10, self.length + 10))
         text_str = "Cartpole Animation"
         
         left, = ax.plot([], [], lw=4)

srcpy/robot_env/one_dof_manipulator.py
@@ -110,7 +110,7 @@ class OneDOFManipulator:
         '''
         A_lin = np.identity(2)
         A_lin[0,1] = dt
-        A_lin[1,0] = -np.round(self.m*self.g*np.cos(state[0])/self.I, 2)
+        A_lin[1,0] = -np.round(self.m*self.g*dt*np.cos(state[0])/self.I, 2)
 
         return A_lin
 